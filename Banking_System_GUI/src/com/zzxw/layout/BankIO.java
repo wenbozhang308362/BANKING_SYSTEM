d4a2d6d548003164c7c3362e7998549baa1dafb3
Mo Xu <monsoon1013@gmail.com>
2015-07-09 15:35:56 -0400

    --

--- Banking_System_GUI/src/com/zzxw/layout/BankIO.java
+++ Banking_System_GUI/src/com/zzxw/layout/BankIO.java
@@ -1,24 +1,26 @@
 package com.zzxw.layout;
-import java.io.FileInputStream;
-import java.io.InputStream;
+import java.io.File;
 import jxl.Cell;
 import jxl.Sheet;
 import jxl.Workbook;
+import jxl.write.Label;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
 
 public class BankIO {
-	String[][] allin;
-
-	public String[][] readexcel(String filePath){
+	File file = new File("info.xls");
+	Workbook wb=null;
+	WritableWorkbook wwb=null;
+	
+//	Reading Method
+	public String[][] readexcel() throws Exception{
 		try {
-			InputStream is = new FileInputStream(filePath);
-			Workbook wb = Workbook.getWorkbook(is);
+			wb = Workbook.getWorkbook(file);
 			Sheet st = wb.getSheet(0);
 			int rows = st.getRows();
 			String[][] info=new String[rows][];
 			for(int i=0;i<rows;i++){
 				Cell[] cells=st.getRow(i);
-//				System.out.println(cells[0].getContents().trim());
-//				System.out.println(cells.length);
 				info[i]=new String[4];
 				for(int j=0;j<cells.length;j++){
 					info[i][j]=cells[j].getContents().trim();
@@ -26,16 +28,54 @@ public class BankIO {
 				}
 			}
 			return info;
-			
 		} catch (Exception e) {
 			System.out.println(e);
 			String[][] info=new String[0][];
 			return info;
-			
-		}	
+		} finally {
+			wb.close();
+		}
 	}
 	
+//	Writing Method
+	public void writeexcel(String[][] info) throws Exception{
+		try {
+			wwb = Workbook.createWorkbook(file);
+			WritableSheet sheet = wwb.createSheet("test sheet 1", 0);
+//			System.out.println(info.length);
+			for(int i=0;i<info.length;i++){
+				for(int j=0;j<info[i].length;j++){
+//					System.out.println("j= "+j);
+					System.out.println(info[i][j]);
+					Label label = new Label(i,j,info[i][j]);
+					sheet.addCell(label);
+					
+				}
+			}
+		} catch (Exception e) {
+//			System.out.println(e);
+			// TODO: handle exception
+		} finally {
+			wwb.write();
+			wwb.close();
+		}
+	}
+	
+	
+	
 	public static void main(String[] args) {
-		new BankIO().readexcel("info.xls");
+		String[][] test={{"1","2"},{"asd","asdfgh"},{"123456","22222"},{"500","1000"}};
+//		try {
+//			new BankIO().readexcel();
+//		} catch (Exception e) {
+//			System.out.println(e);
+//			// TODO: handle exception
+//		}
+		try {
+			new BankIO().writeexcel(test);
+		} catch (Exception e) {
+			System.out.println(e);
+			// TODO: handle exception
+		}
 	}
 }
